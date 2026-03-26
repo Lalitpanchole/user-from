8e3a1ffb5442f0c4df1f4184de9591349f604d18
Lalit Panchole <lalitpanchole8@gmail.com>
2026-03-26 16:41:34 +0530

    Update API endpoint for user profile retrieval

--- Frontend/src/compotents/Home.jsx
+++ Frontend/src/compotents/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       const token = localStorage.getItem("token"); // ✅ fixed
       console.log(token);
       const res = await axios.get(
-        "http://localhost:5000/api/user/profile",
+        "https://user-from-api-backend.onrender.com/api/profile",
         {
           headers: {
           Authorization: `Bearer ${token}`
