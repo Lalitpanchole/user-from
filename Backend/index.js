dcb1adbfd272a90d069ddc7bd6ca9316e2367919
Lalit Panchole <lalitpanchole8@gmail.com>
2026-03-26 16:03:02 +0530

    Refactor user routes and improve logging

--- Backend/index.js
+++ Backend/index.js
@@ -3,17 +3,27 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './DB.js'
 import userRoutes from './routes/userRoutes.js'
+
 dotenv.config()
 const app = express();
+
+// middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+// DB connect
 connectDB();
-app.use("/api/user", userRoutes);
-app.use("api/login", userRoutes);
+
+// ✅ yaha main change
+app.use("/", userRoutes);
+
+// test route
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
 });
-const PORT =  process.env.PORT || 5000;
-app.listen(PORT, (req ,res)=>{
-    console.log(`PORT runing  ${5000}`)
-})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`PORT running ${PORT}`);
+});
